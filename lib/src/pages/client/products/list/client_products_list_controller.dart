bb11c302a08513855461180d0098dae29b1e6325
Emmanuel Chacon <emmanuelechra@gmail.com>
2023-07-09 17:25:58 -0400

    cambiesito

--- lib/src/pages/client/products/list/client_products_list_controller.dart
+++ lib/src/pages/client/products/list/client_products_list_controller.dart
@@ -4,7 +4,7 @@ import '../../../../utils/shared_pref.dart';
 
 class ClientProductsListController {
   late BuildContext context;
-  SharedPref _sharedPref = SharedPref();
+  final SharedPref _sharedPref = SharedPref();
   void init(BuildContext context) {
     this.context = context;
   }
