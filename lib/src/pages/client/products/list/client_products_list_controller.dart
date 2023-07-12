09e0c896166502ce682b0f58b2e1353d66073522
miniproyecto <yurimar2003@gmail.com>
2023-07-11 23:12:47 -0400

    cambios front/flutter en client_products_list_page

--- lib/src/pages/client/products/list/client_products_list_controller.dart
+++ lib/src/pages/client/products/list/client_products_list_controller.dart
@@ -4,11 +4,17 @@ import '../../../../utils/shared_pref.dart';
 class ClientProductsListController {
   late BuildContext context;
   SharedPref _sharedPref = SharedPref();
+  GlobalKey<ScaffoldState> key = new GlobalKey<ScaffoldState>();
+
   void init(BuildContext context) {
     this.context = context;
   }
 
-  logout() {
+  void logout() {
     _sharedPref.logout(context);
+  }
+
+  void openDrawer() {
+    key.currentState?.openDrawer();
   }
 }
